80674317fb55fd993924bdda274ce5c94f6b56b6
Terry Yuen <terryxlife@gmail.com>
2018-08-18 23:29:17 +0800

    v1

---
+++ README.md
@@ -0,0 +1,38 @@
+### Overview
+Oanda Rest is a api wrapper for [oanda rest-v20](http://developer.oanda.com/rest-live-v20/introduction/) with [got](https://github.com/sindresorhus/got) serving as underlying HTTP requests library
+### Getting started
+Requires NodeJS >= v4 
+
+```
+$ npm install @terryx/oanda-rest
+```
+
+### Usage
+```
+const OandaRest = require('@terryx/oanda-rest')
+const api = OandaRest({
+  accountID: 'OANDA_ACCOUNT_ID',
+  endpoint: 'https://api-fxpractice.oanda.com',
+  apiKey: 'API_KEY'
+})
+
+api.accounts().then(res => {
+  // account details
+  console.log(res.account)
+})
+```
+
+### API
+**api.accounts()**
+
+**api.accountsSummary()**
+
+**api.accountsInstruments()**
+
+**api.createOrder([OrderRequest](http://developer.oanda.com/rest-live-v20/order-df/#OrderRequest))**
+
+**api.cancelOrder(OrderID)**
+
+**api.instrumentsCandles = ([InstrumentName](http://developer.oanda.com/rest-live-v20/primitives-df/#InstrumentName), additionalParams = {})**
+
+**api.instrumentsPrice = (additionalParams = {})**
---
+++ index.js
@@ -0,0 +1,36 @@
+const got = require('got')
+
+const constructor = ({ apiKey, endpoint, accountID }) => {
+  const http = got.extend({
+    headers: { 
+      'user-agent': null,
+      'Authorization': `Bearer ${apiKey}` 
+    },
+    baseUrl: endpoint,
+    json: true
+  })
+
+  const method = {}
+
+  const toBody = (promise) => {
+    return promise.then(res => res.body)
+  }
+
+  method.accounts = () => toBody(http.get(`/v3/accounts/${accountID}`))
+
+  method.accountsSummary = () => toBody(http.get(`/v3/accounts/${accountID}/summary`))
+
+  method.accountsInstruments = () => toBody(http.get(`/v3/accounts/${accountID}/instruments`))
+
+  method.createOrder = (body) => toBody(http.post(`/v3/accounts/${accountID}/orders`, { body }))
+
+  method.cancelOrder = (orderID) => toBody(http.put(`/v3/accounts/${accountID}/orders/${orderID}/cancel`))
+
+  method.instrumentsCandles = (instrument, query) => toBody(http.get(`/v3/instruments/${instrument}/candles`, { query }))
+
+  method.instrumentsPrice = (query) => toBody(http.get(`/v3/accounts/${accountID}/pricing`, { query }))
+
+  return method
+}
+
+module.exports = constructor
